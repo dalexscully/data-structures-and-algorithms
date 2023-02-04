2775395b0dc842dd6a5d9da0d16cb99befff2721
Domaine Scully <dalexscully@gmail.com>
2023-02-03 23:19:59 -0500

    Code challenge 34

--- python/data_structures/graph.py
+++ python/data_structures/graph.py
@@ -4,13 +4,46 @@ class Graph:
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.adj_lst = {}
 
-    def some_method(self):
-        # method body here
-        pass
+    def add_node(self, val):
+        node = Vertex(val)
+        if node not in self.adj_lst:
+            self.adj_lst[node] = []
+        return node
+
+    def add_edge(self, node1, node2, weight=None):
+        if node1 not in self.adj_lst or node2 not in self.adj_lst:
+            raise KeyError
+
+        if node1.value == node2.value:
+            self.adj_lst[node1].append(Edge(node2, weight))
+            return
+
+        self.adj_lst[node1].append(Edge(node2, weight))
+        self.adj_lst[node2].append(Edge(node1, weight))
+
+    def get_nodes(self):
+        return self.adj_lst.keys()
+
+    def get_neighbors(self, node):
+        if node in self.adj_lst:
+            return self.adj_lst[node]
+        else:
+            return None
+
+    def size(self):
+        return len(self.adj_lst.keys())
 
 
 class Vertex:
-    pass
+
+    def __init__(self, value):
+        self.value = value
+
+
+class Edge:
+
+    def __init__(self, vertex, weight=None):
+        self.vertex = vertex
+        self.weight = weight
